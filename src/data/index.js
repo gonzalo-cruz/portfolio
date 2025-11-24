3dab9df199160ea8d9d604c342d73f73ed8f4aed
Your Name <gonza.c.gomez03@gmail.com>
2025-11-24 11:29:26 +0100

    añadido proyecto stroke

--- src/data/index.js
+++ src/data/index.js
@@ -1,10 +1,11 @@
 import { churnProject } from './projects/churn.js';
 import { forest } from './projects/forest-fire.js';
 import { rc } from './projects/RC.js';
-
+import { stroke } from './projects/stroke.js';
 
 export const projects = [
   churnProject,
   forest,
   rc,
+  stroke,
 ];
---
+++ src/data/projects/stroke.js
@@ -0,0 +1,10 @@
+export const stroke = {
+  id: 4,
+  title: "Stroke risk predictive analysis",
+  category: "Machine Learning",
+  summary: "Predicting stroke risk with linear models and Artificial Neural Networks",
+  image: "/stroke.jpeg", 
+  tags: ["Python", "MLP", "ANN","EDA", "Classification", "Sklearn"],
+  repoUrl: "https://github.com/gonzalo-cruz/Predictive-stroke-analysis",
+  markdownFile: "stroke.md" 
+};
